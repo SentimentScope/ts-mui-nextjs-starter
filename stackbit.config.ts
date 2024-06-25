b2b035b49dc2343feb37b1ed89d4eed51df201fc
SentimentScope <freshgaltdocs@gmail.com>
2024-06-25 16:40:41 +0700

    Update stackbit.config.ts

--- stackbit.config.ts
+++ stackbit.config.ts
@@ -3,32 +3,30 @@ import { defineStackbitConfig } from '@stackbit/types';
 import { allModels } from './.stackbit/models';
 
 const gitContentSource = new GitContentSource({
-    rootPath: __dirname,
-    contentDirs: ['content'],
-    models: Object.values(allModels),
-    assetsConfig: {
-        referenceType: 'static',
-        staticDir: 'public',
-        uploadDir: 'images',
-        publicPath: '/'
-    }
+	rootPath: __dirname,
+	contentDirs: ['content'],
+	models: Object.values(allModels),
+	assetsConfig: {
+		referenceType: 'static',
+		staticDir: 'public',
+		uploadDir: 'images',
+		publicPath: '/'
+	}
 });
 
 export const sbConfig = defineStackbitConfig({
-    stackbitVersion: '~0.6.0',
-    ssgName: 'nextjs',
-    nodeVersion: '18',
-    contentSources: [gitContentSource],
-    presetSource: {
-        type: 'files',
-        presetDirs: ['.stackbit/presets']
-    },
-    styleObjectModelName: 'ThemeStyle',
-	modelExtensions: [
-	{
-      	name: 'page',
-	type: 'page',
-	urlPath: '/{slug}',
-	  },
-	],
+	stackbitVersion: '~0.6.0',
+	ssgName: 'nextjs',
+	nodeVersion: '18',
+	contentSources: [gitContentSource],
+	presetSource: {
+		type: 'files',
+		presetDirs: ['.stackbit/presets']
+	},
+	styleObjectModelName: 'ThemeStyle',
+	(modelExtensions: [{
+		name: 'page',
+		type: 'page',
+		urlPath: '/{slug}'
+	}])
 });
