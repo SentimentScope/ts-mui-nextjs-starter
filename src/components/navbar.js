fd6c6889a6d89dcd3569c181053ba8e96d82ef4c
SentimentScope <freshgaltdocs@gmail.com>
2024-07-07 14:46:36 +0700

    Update navbar.js

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -8,7 +8,7 @@ const Navbar = () => {
         <li><Link href="/"><a>Home</a></Link></li>
         <li><Link href="/about"><a>About</a></Link></li>
         <li><Link href="/overview"><a>Overview</a></Link></li>
-        <li><Link href="/blog"><a>Blog</a></Link></li>
+        <li><Link href="/blog"><a>Bio</a></Link></li>
         <li><Link href="/contact"><a>Contact</a></Link></li>
       </ul>
       <style jsx>{`
