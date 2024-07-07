bc9f226a1ceeec9e75de285a36fffed5532deb7e
SentimentScope <freshgaltdocs@gmail.com>
2024-07-07 16:44:45 +0700

    Update Link.tsx

--- src/components/atoms/Link.tsx
+++ src/components/atoms/Link.tsx
@@ -2,17 +2,33 @@ import * as React from 'react';
 import NextLink from 'next/link';
 import type * as types from 'types';
 
-export type Props = types.Link & types.StackbitFieldPath & { className?: string; sx?: { [key: string]: any } };
+export type Props = types.Link & types.StackbitFieldPath & {
+  className?: string;
+  sx?: { [key: string]: any };
+};
 
 export const Link: React.FC<Props> = (props) => {
-    const { className, label, url, underline = 'always', color = 'primary', sx, 'data-sb-field-path': fieldPath } = props;
-    const annotations = fieldPath ? [fieldPath, `${fieldPath}.url#@href`].join(' ').trim() : null;
+  const {
+    className,
+    label,
+    url,
+    underline = 'always',
+    color = 'primary',
+    sx,
+    'data-sb-field-path': fieldPath
+  } = props;
 
-    return (
-        <NextLink href={url} passHref>
-            <a className={className} style={{ textDecoration: underline === 'none' ? 'none' : 'underline' }}>
-                <span data-sb-field-path=".label">{label}</span>
-            </a>
-        </NextLink>
-    );
+  const annotations = fieldPath ? [fieldPath, `${fieldPath}.url#@href`].join(' ').trim() : null;
+
+  return (
+    <NextLink href={url} passHref>
+      <span
+        className={className}
+        style={{ textDecoration: underline === 'none' ? 'none' : 'underline' }}
+        data-sb-field-path=".label"
+      >
+        {label}
+      </span>
+    </NextLink>
+  );
 };
