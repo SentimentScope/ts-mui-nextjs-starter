f7942642f50c1f843cf899d4cee9bf637862d3a2
SentimentScope <freshgaltdocs@gmail.com>
2024-07-07 16:07:32 +0700

    Update Link.tsx

--- src/components/atoms/Link.tsx
+++ src/components/atoms/Link.tsx
@@ -2,8 +2,6 @@ import * as React from 'react';
 import NextLink from 'next/link';
 import type * as types from 'types';
 
-import MuiLink from '@mui/material/Link';
-
 export type Props = types.Link & types.StackbitFieldPath & { className?: string; sx?: { [key: string]: any } };
 
 export const Link: React.FC<Props> = (props) => {
@@ -11,8 +9,10 @@ export const Link: React.FC<Props> = (props) => {
     const annotations = fieldPath ? [fieldPath, `${fieldPath}.url#@href`].join(' ').trim() : null;
 
     return (
-        <MuiLink component={NextLink} href={url} className={className} underline={underline} color={color} sx={{ ...sx }} data-sb-field-path={annotations}>
-            <span data-sb-field-path=".label">{label}</span>
-        </MuiLink>
+        <NextLink href={url} passHref>
+            <a className={className} style={{ textDecoration: underline === 'none' ? 'none' : 'underline' }}>
+                <span data-sb-field-path=".label">{label}</span>
+            </a>
+        </NextLink>
     );
 };
